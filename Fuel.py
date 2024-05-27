399ec2a4dc769f88c65aba736d77732c2925fe2d
Luiz Vasconcelos <luizantoniogclvasconcelos22@hotmail.com>
2024-05-26 21:58:27 -0300

    traducao codigo para pt-br
    kkkkkkkkkk

--- Fuel.py
+++ Fuel.py
@@ -1,4 +1,4 @@
-#pip install. pip install pandas numpy matplotlib seaborn plotly scikit-learn dash
+# pip install pandas numpy matplotlib seaborn plotly scikit-learn dash
 
 print('hello world')
 
@@ -8,113 +8,84 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 import plotly.express as px 
 
-
-
-
-fuel = pd.read_csv("FUEL.csv")
+# Ler o arquivo CSV com a opção low_memory=False
+fuel = pd.read_csv("FUEL.csv", low_memory=False)
 print(fuel.head())
-print (fuel.tail ())
+print(fuel.tail())
 
 # Verifique os valores ausentes
-
 missing_values = fuel.isnull().sum()
-print (missing_values)
+print(missing_values)
 
-# Assumindo que o DF é o seu quadro de dados
+# Criação de DataFrame de exemplo
 df = pd.DataFrame({
-    'A': [1, 2, 3],
-    'B': [4, 5, None],
-    'C': [6, None, 8]
+    'ID do Veículo': [1, 2, 3],
+    'Ano': [4, 5, None],
+    'Fabricante': [6, None, 8]
 })
 
-print("Before dropping columns with NaN values:")
+print("Antes de descartar colunas com valores NaN:")
 print(df)
 
-# Soltar colunas com valores de NaN
+# Remover colunas com valores NaN
 df = df.dropna(axis=1)
 
-print("After dropping columns with NaN values:")
+print("Depois de eliminar colunas com valores NaN:")
 print(df)
 
-# Excluindo coluna com valores de NaN
-
-del fuel['transmission_type']
-del fuel['range_ft2']
-fuel.shape
-(38113, 79)
-fuel.head()
-
-del fuel['drive']
-fuel.info()
-
-del fuel['supercharger']
-del fuel['fuel_type_2']
-fuel.describe()
-
+# Remover colunas com valores NaN do DataFrame 'fuel'
+fuel.drop(columns=['tipo_de_transmissão', 'consumo_cidade_mpg_arredondado_ft2', 'tração', 'supercharger', 'combustível_2'], inplace=True)
+print(fuel.shape)
+print(fuel.info())
 
 # Distribuição de veículos por ano
-
-df = fuel
-
 plt.figure(figsize=(12, 6))
-sns.countplot(x='year', data=df, palette='viridis')
+sns.countplot(x='ano', data=fuel, palette='viridis')
 plt.xticks(rotation=90)
-plt.title('Distribution of Vehicles by Year')
-plt.xlabel('Year')
-plt.ylabel('Number of Vehicles')
+plt.title('Distribuição de Veículos por Ano')
+plt.xlabel('Ano')
+plt.ylabel('Número de Veículos')
 plt.tight_layout()
 plt.show()
 
 # Distribuição de veículos (Top 20)
-
 plt.figure(figsize=(12, 6))
-top_makes = df['make'].value_counts().nlargest(20)
+top_makes = fuel['fabricante'].value_counts().nlargest(20)
 sns.barplot(x=top_makes.index, y=top_makes.values, palette='viridis')
 plt.xticks(rotation=90)
-plt.title('Top 20 Vehicle Makes')
-plt.xlabel('Make')
-plt.ylabel('Number of Vehicles')
+plt.title('As 20 Principais Marcas de Veículos')
+plt.xlabel('Fabricante')
+plt.ylabel('Número de Veículos')
 plt.tight_layout()
 plt.show()
 
 # Distribuição dos tipos de combustível
-
 plt.figure(figsize=(12, 6))
-sns.countplot(x='fuel_type', data=df, palette='viridis')
+sns.countplot(x='tipo_de_combustível', data=fuel, palette='viridis')
 plt.xticks(rotation=45)
-plt.title('Distribution of Fuel Types')
-plt.xlabel('Fuel Type')
-plt.ylabel('Number of Vehicles')
+plt.title('Distribuição de Tipos de Combustível')
+plt.xlabel('Tipo de Combustível')
+plt.ylabel('Número de Veículos')
 plt.tight_layout()
 plt.show()
 
 # Distribuição de cilindros de motor
-
 plt.figure(figsize=(12, 6))
-sns.countplot(x='engine_cylinders', data=df, order=df['engine_cylinders'].value_counts().index, palette='viridis')
+sns.countplot(x='cilindros_do_motor', data=fuel, order=fuel['cilindros_do_motor'].value_counts().index, palette='viridis')
 plt.xticks(rotation=45)
-plt.title('Distribution of Engine Cylinders')
-plt.xlabel('Engine Cylinders')
-plt.ylabel('Number of Vehicles')
+plt.title('Distribuição de Cilindros de Motor')
+plt.xlabel('Cilindros do Motor')
+plt.ylabel('Número de Veículos')
 plt.tight_layout()
 plt.show()
-
 
 # Distribuição de veículos por classe
-
 plt.figure(figsize=(12, 6))
-vehicle_classes = df['class'].value_counts()
+vehicle_classes = fuel['classe'].value_counts()
 sns.barplot(x=vehicle_classes.index, y=vehicle_classes.values, palette='viridis')
 plt.xticks(rotation=90)
-plt.title('Distribution of Vehicles by Class')
-plt.xlabel('Class')
-plt.ylabel('Number of Vehicles')
+plt.title('Distribuição de Veículos por Classe')
+plt.xlabel('Classe')
+plt.ylabel('Número de Veículos')
 plt.tight_layout()
 plt.show()
-
-
-
-
-
-
-
